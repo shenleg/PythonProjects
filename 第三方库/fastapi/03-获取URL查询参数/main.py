46be29181c78e63302d7942c6e1c838e1843e0c2
张云辉 <admin@example.com>
2023-07-27 00:53:02 +0800

    update fastapi

--- 第三方库/fastapi/03-获取URL查询参数/main.py
+++ 第三方库/fastapi/03-获取URL查询参数/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Query
 import uvicorn
 
 app = FastAPI()
@@ -7,7 +9,9 @@ app = FastAPI()
 例子：
 1. http://127.0.0.1:5000/user/1?name=张三&age=18
 2. http://127.0.0.1:5000/hobby?id=1&hobby=eat&hobby=烫头
+3. http://127.0.0.1:5000/items?item-query=1&@@@=eat&$$$=烫头
 """
+
 
 # 和路径参数混用
 @app.get("/user/{user_id}")
@@ -17,6 +21,7 @@ def get_user(user_id: int, name: str, age: int):
     print("age type is", type(age))
     return f'id is {user_id} and name is {name} and age is {age}!'
 
+
 # 查询参数默认值，可以不用传
 @app.get("/user/{user_id}")
 def get_user(user_id: int, name: str, age: int = 0):
@@ -25,11 +30,19 @@ def get_user(user_id: int, name: str, age: int = 0):
     print("age type is", type(age))
     return f'id is {user_id} and name is {name} and age is {age}!'
 
+
+# 获取多个同名称参数
 @app.get('/hobby')
-def get_hobby(user_id: int, hobby: list):
+def get_hobby(user_id: int, hobby: List[str] = Query()):
     print("user_id type is", type(user_id))
     print("hobby type is", type(hobby))  # list 类型
     return f'id is {user_id} and name is {hobby}!'
+
+
+# 起别名
+@app.get('/items')
+def get_hobby(item1: str = Query(alias="item-query"), item2: str = Query(alias="@@@"), item3: str = Query(alias="$$$")):
+    return {"item1": item1, "item2": item2, "item3": item3}
 
 
 if __name__ == "__main__":
--- 第三方库/fastapi/查询参数校验/main.py
+++ 第三方库/fastapi/查询参数校验/main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Union, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import uvicorn
 
 app = FastAPI()
@@ -17,33 +17,52 @@ class Name(str, Enum):
 例子：
 1. http://127.0.0.1:5000/book_id/2
 2. http://127.0.0.1:5000/users/a
-3. http://127.0.0.1:5000/user/张三
+3. http://127.0.0.1:5000/users2
+4. http://127.0.0.1:5000/users3/tom444
+5. http://127.0.0.1:5000/items
 
 3.8 3.9 3.10 区别
-# Optional[str] 和 str | None 等价
->>> name: Optional[str] = "古明地觉"
->>> name: str | None = "古明地觉"
-
-# Union[int, str] 和 int | str 等价
->>> age: Union[int, str] = 17
->>> age: int | str = 17
+Optional[str] 和 str | None 等价
+Union[int, str] 和 int | str 等价
 这种写法 3.10 才开始引入，通过 from __future__ import annotations 也可以在 3.9 里面使用，而 3.8 是不支持的
+
+Query 不支持给"路径参数"做校验！
 """
 
 
-# 单个类型
+# 单个类型，必传值
 @app.get('/book_id/{book_id}')
 def get_book_id(book_id: int):
     print("book_id type is", type(book_id))
     return f'book_id is {book_id}!'
 
 
-# 多选一类型、默认值
+# 多选一类型
 @app.get('/users/{user_id}')
-def get_user(user_id: Union[int, str], user_name: Optional[str] = None):
+def get_user(user_id: Union[int, str]):
+    print("user_id type is", type(user_id))
+    return f'user_id is {user_id}!'
+
+
+# 可选值
+@app.get('/users2')
+def get_user2(user_id: int = Query(12), user_name: Optional[str] = None):
     print("user_id type is", type(user_id))
     print("user_name type is", type(user_name))
     return f'user_id is {user_id} and user_name is {user_name}!'
+
+
+# 长度、匹配限制
+@app.get('/users3')
+def get_user3(user_name: str = Query(min_length=2, max_length=4, pattern=r"^tom")):
+    print("user_name type is", type(user_name))
+    return f'user_name is {user_name}!'
+
+
+# 数值校验
+@app.get('/items')
+def get_user3(item1: int = Query(gt=5), item2: int = Query(le=7), item3: int = Query(ge=10, le=10)):
+    return {"item1": item1, "item2": item2, "item3": item3}
 
 
 # 枚举类型
