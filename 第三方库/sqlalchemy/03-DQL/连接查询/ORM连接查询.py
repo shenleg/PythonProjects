4d89ebb5bcc8ebb69a22b35f12be40de3d2dd00d
张云辉 <admin@example.com>
2023-08-11 00:34:51 +0800

    update sqlalchemy

--- 第三方库/sqlalchemy/03-DQL/连接查询/ORM连接查询.py
+++ 第三方库/sqlalchemy/03-DQL/连接查询/ORM连接查询.py
@@ -1,57 +1,26 @@
-from sqlalchemy import Column, Integer, String, or_
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, or_, select
+from sqlalchemy.orm import declarative_base, relationship, mapped_column, Mapped
 
 from 第三方库.sqlalchemy.common.database import UsingAlchemy
 
 Base = declarative_base()
 
 
-class Student(Base):
+class Child(Base):
     # 定义表名
-    __tablename__ = 'student'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(32))
-    age = Column(Integer)
-    t_id = Column(Integer)
-
-    teacher = relationship("Teacher", back_populates="user")
+    __tablename__ = 'child'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(32))
+    p_id: Mapped[int] = mapped_column(Integer)
 
 
-class Teacher(Base):
+class Parent(Base):
     # 定义表名
-    __tablename__ = 'teacher'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(32))
-    age = Column(Integer)
-
+    __tablename__ = 'parent'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(32))
 
 
 with UsingAlchemy() as ua:
-    # 内连接
-    # 结果只包含一个
-    query = ua.session.query(Student).join(Teacher, Student.t_id == Teacher.id)
-    print(query)
-    # 补上其他结果
-    query = ua.session.query(Student).join(Teacher,
-                                           Student.t_id == Teacher.id).add_entity(
-        Teacher)
-    print(query)
-
-    # 外连接
-    query = ua.session.query(Student).outerjoin(Teacher,
-                                                Student.t_id == Teacher.id)
-    print(query)
-    query = ua.session.query(Student).outerjoin(Teacher,
-                                                Student.t_id == Teacher.id).add_entity(
-        Teacher)
-    print(query)
-
-    # 加上筛选
-    query = ua.session.query(Student).outerjoin(
-        Teacher, Student.t_id == Teacher.id
-    ).filter(
-        or_(Student.id == 1, Student.id == 3)
-    ).add_entity(Teacher)
-    print(query)
-    for student, teacher in query.all():
-        print(student, teacher)
+    stmt = select(Parent, Child)
+    print(stmt)
